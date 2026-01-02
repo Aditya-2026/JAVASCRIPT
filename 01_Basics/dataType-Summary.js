b0285de4157aa28fe3f1f11166c491dd3db85ea0
Aditya-2026 <adityadahake2004@gmail.com>
2026-01-02 05:21:06 +0000

    DataType Summary

---
+++ 01_Basics/dataType-Summary.js
@@ -0,0 +1,28 @@
+// primitive - call by value
+// string, Number, Boolean, null, undefined, symbol ,BigInt
+
+// Refernce (Non primitive)
+// arrays, objects, functions
+
+// js - dynamically typed language no need to mention datatype is determined at runtime
+
+const id = Symbol('123')
+const id2 = Symbol('123')
+console.log(id === id2);
+console.log(id == id2);
+const bigNumber = 4444444234553366666666666n
+
+//objects
+{
+    name : "Aditya"
+    age : 22
+}
+
+const myfunction = function(){
+    console.log("Hello World");
+    
+}
+console.log(typeof bigNumber);
+
+
+ 
